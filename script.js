b5f114a408fa6cf4661ed57f9e5eef8962119225
iceburg7 <ecburgess1@gmail.com>
2014-09-23 09:16:47 -0400

    more images and some links

--- script.js
+++ script.js
@@ -57,6 +57,55 @@ $(document).ready(function(){
 	
 	
 	
+	$(".background").on("event", function(){
+	
+		if(x==="1w"){
+			left("1s");
+			center("2w");
+			right("1n");
+		}
+		
+		if(x==="1s"){
+			left("1e");
+			center("1s");
+			right("1w");
+		}
+		
+		if(x==="1n"){
+			left("1w");
+			center("1n");
+			right("1e");
+		}
+		
+		if(x==="1e"){
+			left("1n");
+			center("10e");
+			right("1s");
+		}
+		
+		if(x==="2n"){
+			left("2w");
+			center("2n");
+			right("2e");
+		}
+		
+		if(x==="2w"){
+			left("2s");
+			center("3w");
+			right("2n");
+		}
+		
+		if(x==="2s"){
+			left("2e");
+			center("2s");
+			right("2w");
+		}
+		
+		if(x==="2e"){
+			left("2n");
+			center("1e");
+			right("1s");
+		}
 	
 	
 	
@@ -65,6 +114,32 @@ $(document).ready(function(){
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	});
+	
+	$(".background").trigger("event");
 	
 	
 });
